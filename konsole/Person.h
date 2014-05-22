5eb5165ffc8c6dc5fea7fc9492f998c845c325da
Kushal <kushaldeveloper@gmail.com>
2014-05-22 17:03:03 -0500

    add person class

---
+++ konsole/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  konsole
+//
+//  Created by kushal on 5/22/14.
+//  Copyright (c) 2014 kus. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ konsole/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  konsole
+//
+//  Created by kushal on 5/22/14.
+//  Copyright (c) 2014 kus. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
