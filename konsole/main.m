b6b1d2c66c909fe9f6788870355a6eda4662d516
kushal <kushal@stem1.local>
2014-05-22 16:45:34 -0500

    Initial Commit

---
+++ konsole/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  konsole
+//
+//  Created by kushal on 5/22/14.
+//  Copyright (c) 2014 kus. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
